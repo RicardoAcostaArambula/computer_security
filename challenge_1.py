8aed2056f57f6fc7a349d259513ea20c681fbb18
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-10-15 21:29:53 -0600

    adds challenge 1 python script (it needs to be tested)

---
+++ challenge_1.py
@@ -0,0 +1,38 @@
+import os
+import stat
+
+challengeDirectory = "/root/challenge"
+subDirectories = ["dir1", "dir2", "dir3", "dir4"]
+key = "the secret key is: c0mpu73r 23cur17y"
+fileWithKey = "dir5"
+hintMessage ="The key is in one of the files, but not here..."
+def setupChallenge():
+    os.makedirs(challengeDirectory, exist_ok=True)
+    os.chdir(challengeDirectory)
+
+    #creating subdirectories 
+    for subdirectory in subDirectories:
+        os.makedir(subdirectory, exist_ok=True)
+    
+
+        #creating files
+        for i in range(1, 4):
+            fileName = f"file{i}.txt"
+            with open(fileName, "w") as file:
+                file.write(hintMessage)
+    #choosing one directory to place the password
+    keyDirectory = os.path.join(challengeDirectory, "dir5")
+    os.chdir(keyDirectory)
+
+    with open(keyDirectory, "w") as file:
+        file.write(key)
+    protectFiles()
+def protectFiles():
+    for root, dirs , files in os.walk(challengeDirectory):
+        for directoryName in dirs:
+            os.chmod(os.path.join(root, directoryName), stat.SIRWXU) 
+        for fileName in files:
+            os.chmod(os.path.join(root, fileName), stat.S_IRUSR)
+if __name__ == "__main__":
+    setupChallenge()
+    print(f"Challenge setup complete! Files created under {challengeDirectory}")
