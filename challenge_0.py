ab94432706c494d1c1840b9247e10eb5e2473b22
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-11-13 12:32:46 -0700

    adds comemnts

--- challenge_0.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-# This script must be run inside ther user not root
-#Script that creates a file with the root password under home directory
-rootPassword = "toor"
-homePath = Path.home()
-fileName = "my_password"
-filePath = homePath / fileName
-filePath.write_text(f"root password: {rootPassword}\n")
-print(f"File created at {filePath} with the root password")
