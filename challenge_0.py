a9bfd25a525e77956085cb43e41f9154b85edee2
RicardoAcostaArambula <acostaricardo463@gmail.com>
2024-11-17 17:24:14 -0700

    fixed challenge 0

--- challenge_0.py
+++ challenge_0.py
@@ -1,8 +1,20 @@
+import os
 from pathlib import Path
-#Script that creates a file with the root password under home directory
-rootPassword = "dees"
-homePath = Path.home()
-fileName = "my_password"
-filePath = homePath / fileName
-filePath.write_text(f"root password: {rootPassword}\n")
-print(f"File created at {filePath} with the root password")
+import subprocess
+import pwd
+
+root_password = "dees"
+target_user = "Joe"
+target_password = "joe_password"
+
+subprocess.run(f"sudo useradd -m {target_user} && echo '{target_user}:{target_password}' | sudo chpasswd", shell=True)
+subprocess.run(["usermod", "-aG", "sudo", target_user], check=True)
+print(f"Added {target_user} to the sudo group.")
+home_path = Path(f"/home/{target_user}")
+file_name = "my_password"
+file_path = home_path / file_name
+file_path.write_text("root password: {root_password}\nMy password: Doe")
+print(f"File created at {file_path} with the root password")
+os.chown(file_path, uid=pwd.getpwnam(target_user).pw_uid, gid=pwd.getpwnam(target_user).pw_gid)
+
+subprocess.run(["su", "-", target_user])
