a8cef52d059c8258374ccae51ed9fe24d4dcacd3
Kevin Guerra <85200373+kevinguerra76@users.noreply.github.com>
2024-11-24 22:09:59 -0700

    Update startup.py

--- startup.py
+++ startup.py
@@ -17,7 +17,6 @@ Finally, to execute type on terminal
 sudo python3 startup.py
 '''
 import os
-import subprocess
 
 def apt_upgrade():
     os.system('sudo apt update')
@@ -35,6 +34,33 @@ def install_ghidra():
     print('#'*50)
     print('Ghidra installed')
     print('To open, use the command ./ghidraRun through the ghidra/ directory')
+
+def install_tshark():
+    os.system('sudo apt install tshark')
+    print('tshark will be used to track you!')
+
+    os.system('sudo tshark > your-network-info')
+    pass
+
+def clone_project():
+    os.system('git clone https://github.com/RicardoAcostaArambula/computer_security/')
+    os.system('sudo cp -r computer_security CTF')
+    os.system('sudo rm -r computer_security/')
+    os.system('cd CTF/ && sudo git remote remove origin')
+    os.system('sudo mv CTF/Ricardo/ CTF/Challenge1/')
+    os.system('sudo mv CTF/Kevin/ CTF/Challenge2/')
+    os.system('sudo mv CTF/Jazmin/ CTF/Challenge3/')
+    os.system('sudo mv CTF/Adrian/ CTF/Challenge4/')
+    os.system('sudo mv CTF/Jorge/ CTF/Challenge5/')
+    # os.system('chdir ~')
+
+def create_user():
+    os.system('sudo useradd -m joe')
+    os.system('echo "joe:Doe" | sudo chpasswd')
+
+def open_html():
+    # need to push to main and open from dir
+    os.system('firefox CTF/WebUI/index.html')
     
 def install_pynput():
     print("Now installing pynput library.")
@@ -46,9 +72,16 @@ def start_logger():
     subprocess.Popen(["python3", "logger.py"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     
 def main():
-    #apt_upgrade()
+    def main():
+    apt_upgrade()
     install_ghidra()
-    #start_logger()
+    install_tshark()
+    clone_project()
+    create_user()
+    open_html()
+    start_logger()
+
+
 
 if __name__ == '__main__':
     main()
